61ba59dac9511c4c6b8695b51791da51b84b109e
Spencer Lindemuth <lindemut@gmail.com>
2019-03-19 23:11:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -14,6 +14,10 @@ def display_card_total(total)
   puts "Your cards add up to #{total}"
 end
 
+def display_dealer_total(total)
+  puts "The Dealers cards add up to #{total}"
+end
+
 def prompt_user
   # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
@@ -29,12 +33,21 @@ def end_game(total)
   puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
+def dealer_wins(total_dealer, total_player)
+  puts "The Dealer wins this round with #{total_dealer} to your #{total_player}."
+  exit
+end
+
+def player_wins(total_player, total_dealer)
+  puts "You beat the dealer #{total_player} to #{total_dealer}!"
+  exit
+end
+
 def initial_round
   # code #initial_round here
   hand_a = deal_card
   hand_b = deal_card
   hand_total = hand_a + hand_b
-  display_card_total(hand_total)
   return hand_total
 end
 
@@ -54,10 +67,25 @@ def hit?(current_hand)
   end
 end
 
+def dealer_hit?(dealer_hand)
+  if dealer_hand < 17
+    new_card = deal_card
+    dealer_hand += new_card
+  else
+    dealer_hand
+  end
+end
+
 def invalid_command
   # code invalid_command here
   puts "Please enter a valid command"
 end
+
+def check_win(dealer, player)
+  if(dealer > player && dealer <= 21)
+      dealer_wins(dealer, player)
+  elsif(dealer < player && player <= 21)
+      player_wins(player, dealer)
 
 #####################################################
 # get every test to pass before coding runner below #
@@ -65,11 +93,18 @@ end
 
 def runner
   # code runner here
+  turn = 0
   welcome
   card_total = initial_round
+  display_card_total(card_total)
+  dealer_total = initial_round
+  display_dealer_total(dealer_total)
   while card_total <= 21
     card_total = hit?(card_total)
     display_card_total(card_total)
+    dealer_total = dealer_hit?(dealer_total)
+    display_dealer_total(dealer_total)
+    check_win(dealer_total, card_total)
   end
   end_game(card_total)
 end
